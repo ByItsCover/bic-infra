b5bc6dc2aecc54b19b283d23ff9977c5a86263cd
Kalamojo <kojo22444488888888@gmail.com>
2026-01-12 21:58:41 -0500

    Adding ECR repository

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+# RDS
+
 output "db_master_username" {
   value = var.rds_master_username
 }
@@ -5,4 +7,10 @@ output "db_master_username" {
 output "db_master_password" {
   value     = aws_secretsmanager_secret_version.password.secret_string
   sensitive = true
+}
+
+# ECR
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.repo.repository_url
 }
