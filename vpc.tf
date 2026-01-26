31988a3d07af91fefc8ccdf43781c283e7dd701e
Kalamojo <kojo22444488888888@gmail.com>
2026-01-26 09:16:40 -0500

    Fixed subnet reference and removed unneccessary retrieval

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,12 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "subnet" {
-  vpc_id = data.aws_vpc.default.id
+/*
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
 }
+*/
+
