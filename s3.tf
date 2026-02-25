57087485e0e045760ea261fd96aa57ba8a308299
Kalamojo <kojo22444488888888@gmail.com>
2026-02-24 23:05:13 -0500

    Fixed bucket reference

--- s3.tf
+++ s3.tf
@@ -4,6 +4,6 @@ resource "aws_s3_bucket" "cover_bucket" {
 }
 
 resource "aws_s3_object" "coverdb" {
-  bucket = aws_s3_bucket.this_bucket.id
+  bucket = aws_s3_bucket.cover_bucket.id
   key    = "${var.db_folder_name}/"
 }
