99a0a83aa6167a80fc84431ffa9a6750d84de9c0
Kalamojo <kojo22444488888888@gmail.com>
2026-04-05 18:06:41 -0400

    Removing origin slash

--- api_gateway.tf
+++ api_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_apigatewayv2_api" "search_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = ["http://${aws_s3_bucket.site_bucket.id}.s3-website.${var.aws_region}.amazonaws.com/"]
+    allow_origins = ["http://${aws_s3_bucket.site_bucket.id}.s3-website.${var.aws_region}.amazonaws.com"]
     allow_methods = ["POST", "GET", "OPTIONS"]
     allow_headers = ["content-type"]
     max_age = 300
