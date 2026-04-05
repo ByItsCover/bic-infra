d660561c6f044a99daff714acd456a0d1db63d38
Kalamojo <kojo22444488888888@gmail.com>
2026-04-05 14:51:23 -0400

    Adding cors config for site temporarily

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,4 +1,11 @@
 resource "aws_apigatewayv2_api" "search_api" {
   name          = "search-api"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["http://${aws_s3_bucket.site_bucket.id}.s3-website.${var.aws_region}.amazonaws.com/"]
+    allow_methods = ["POST", "GET", "OPTIONS"]
+    allow_headers = ["content-type"]
+    max_age = 300
+  }
 }
