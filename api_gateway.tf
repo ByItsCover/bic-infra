0fdcf8e1814659e58e8574870db65d759589ef0d
Kalamojo <kojo22444488888888@gmail.com>
2026-04-25 12:34:28 -0400

    Allowing www site cors

--- api_gateway.tf
+++ api_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_apigatewayv2_api" "search_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = ["https://${var.domain_name}"]
+    allow_origins = ["https://${var.domain_name}", "https://www.${var.domain_name}"]
     allow_methods = ["POST", "GET", "OPTIONS"]
     allow_headers = ["content-type"]
     max_age       = 300
