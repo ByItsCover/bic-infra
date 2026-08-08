170f841f199eef8c67babe092fb4c26870d96e95
Kalamojo <kojo22444488888888@gmail.com>
2026-08-08 14:56:22 -0400

    Setting desired vcpus and adding sqs permissions to s3 bucket

--- batch.tf
+++ batch.tf
@@ -36,6 +36,7 @@ resource "aws_batch_compute_environment" "spot" {
       prefix           = "${env.name}-${env.compute_type}-batch-fleet-"
       instance_types   = env.instance_types
       compute_type     = env.compute_type
+      desired_vcpus    = env.desired_vcpus
       template_id      = aws_launch_template.batch_launch_template[env.name].id
       template_version = aws_launch_template.batch_launch_template[env.name].latest_version
     }
@@ -52,7 +53,7 @@ resource "aws_batch_compute_environment" "spot" {
 
     max_vcpus     = var.max_batch_vcpus
     min_vcpus     = 0
-    desired_vcpus = 0
+    desired_vcpus = each.value.desired_vcpus
 
     security_group_ids = [
       aws_security_group.batch.id
