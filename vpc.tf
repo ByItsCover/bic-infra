b410ebd19582b79b7dbcd4b77c589cefa13c734f
Kalamojo <kojo22444488888888@gmail.com>
2026-08-08 22:34:14 -0400

    Using distinct direct

--- vpc.tf
+++ vpc.tf
@@ -20,5 +20,5 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id       = data.aws_vpc.default.id
   service_name = "com.amazonaws.${var.aws_region}.s3"
 
-  route_table_ids = [distinct(data.aws_route_table.all_tables[*].id)]
+  route_table_ids = distinct(data.aws_route_table.all_tables[*].id)
 }
