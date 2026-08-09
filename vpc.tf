5c7f96e2292e086df69005ece96721e223b42b0d
Kalamojo <kojo22444488888888@gmail.com>
2026-08-08 22:47:58 -0400

    I'm gonna crash out

--- vpc.tf
+++ vpc.tf
@@ -19,5 +19,5 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id       = data.aws_vpc.default.id
   service_name = "com.amazonaws.${var.aws_region}.s3"
 
-  route_table_ids = aws_route_tables.all_tables.ids
+  route_table_ids = data.aws_route_tables.all_tables.ids
 }
