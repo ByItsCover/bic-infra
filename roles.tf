fdeb8a16c4bf1fb793608bedb44f07c9fa77d495
Kalamojo <kojo22444488888888@gmail.com>
2026-08-13 16:14:30 -0400

    Allowing eventbridge to schedule listopia job

--- roles.tf
+++ roles.tf
@@ -195,7 +195,10 @@ data "aws_iam_policy_document" "scheduler_batch_policy" {
       "batch:TerminateJob"
     ]
 
-    resources = [aws_batch_job_queue.queue[var.learn_batch_name].arn]
+    resources = [
+      aws_batch_job_queue.queue[var.learn_batch_name].arn,
+      aws_batch_job_queue.queue[var.listopia_batch_name].arn
+    ]
   }
 }
 
